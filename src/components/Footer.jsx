386cd8f292dc5062e325d10292007bc4d50b450f
Shaiful Islam Emon <eshaifulislam@gmail.com>
2025-10-16 21:30:34 +0600

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full text-justify md:w-2/3 text-gray-600 justify-">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis
+            libero deleniti eligendi soluta consequatur fugit, repellendus
+            explicabo quos facere dolores, quidem, minima neque dolorum minus
+            nesciunt earum reiciendis aut veritatis.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>HOME</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-212-456-7890</li>
+            <li>contact@foreveryou.com</li>
+          </ul>
+        </div>
+      </div>
+      <div className="text-gray-700">
+        <hr className="border-gray-200" />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ forever.com - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
